3a65259b0869235f06a69711889d304e61831216
Erik Antonyan <46004254+Erik1994@users.noreply.github.com>
2024-06-02 13:02:01 +0400

    Add build-logic module and implement AndroidApplicationConventionPlugin

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.example.runtracker.buildlogic"
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.android.tools.common)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.room.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = "run.tracker.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+    }
+}
