5267c79537673150c278afcb8329ccfef0146ba1
Erik Antonyan <46004254+Erik1994@users.noreply.github.com>
2024-06-17 09:21:20 +0400

    Add necessary fields in RunTrackingViewModel

--- run/presentation/src/main/java/com/example/run/presentation/tracking/RunTrackingViewModel.kt
+++ run/presentation/src/main/java/com/example/run/presentation/tracking/RunTrackingViewModel.kt
@@ -3,15 +3,18 @@ package com.example.run.presentation.tracking
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.run.domain.RunningTracker
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.receiveAsFlow
-import timber.log.Timber
+import kotlinx.coroutines.flow.stateIn
 
 class RunTrackingViewModel(
     private val runningTracker: RunningTracker
@@ -23,10 +26,21 @@ class RunTrackingViewModel(
     private val eventChannel = Channel<RunTrackingEvent>()
     val events = eventChannel.receiveAsFlow()
 
-    private val _hasLocationPermission = MutableStateFlow(false)
+    private val shouldTrack = snapshotFlow { state.shouldTrack }
+        .stateIn(viewModelScope, SharingStarted.Lazily, state.shouldTrack)
+
+    private val hasLocationPermission = MutableStateFlow(false)
+
+    private val isTracking = combine(
+        shouldTrack,
+        hasLocationPermission
+    ) { shouldTrack, hasPermission ->
+        shouldTrack && hasPermission
+    }.stateIn(viewModelScope, SharingStarted.Lazily, false)
+
 
     init {
-        _hasLocationPermission
+        hasLocationPermission
             .onEach { hasPermission ->
                 if (hasPermission) {
                     runningTracker.startObservingLocation()
@@ -35,21 +49,59 @@ class RunTrackingViewModel(
                 }
             }.launchIn(viewModelScope)
 
+        isTracking
+            .onEach { isTracking ->
+                runningTracker.setIsTracking(isTracking)
+            }.launchIn(viewModelScope)
+
         runningTracker
             .currentLocation
             .onEach { location ->
-                Timber.d("New Location: $location")
+                state = state.copy(
+                    currentLocation = location?.location
+                )
+            }.launchIn(viewModelScope)
+
+        runningTracker
+            .runData
+            .onEach { runData ->
+                state = state.copy(
+                    runData = runData
+                )
+            }.launchIn(viewModelScope)
+
+        runningTracker
+            .elapsedTime
+            .onEach { elapsedTime ->
+                state = state.copy(
+                    elapsedTime = elapsedTime
+                )
             }.launchIn(viewModelScope)
     }
 
     fun onAction(action: RunTrackingAction) {
         when (action) {
-            RunTrackingAction.OnBackClick -> {}
-            RunTrackingAction.OnFinishRunClick -> {}
-            RunTrackingAction.OnResumeRunClick -> {}
-            RunTrackingAction.OnToggleRunClick -> {}
+            RunTrackingAction.OnBackClick -> {
+                state = state.copy(
+                    shouldTrack = false
+                )
+            }
+            RunTrackingAction.OnFinishRunClick -> {
+
+            }
+            RunTrackingAction.OnResumeRunClick -> {
+                state = state.copy(
+                    shouldTrack = true
+                )
+            }
+            RunTrackingAction.OnToggleRunClick -> {
+                state = state.copy(
+                    hasStartedRunning = true,
+                    shouldTrack = state.shouldTrack.not()
+                )
+            }
             is RunTrackingAction.SubmitLocationPermissionInfo -> {
-                _hasLocationPermission.value = action.acceptedLocationPermission
+                hasLocationPermission.value = action.acceptedLocationPermission
                 state = state.copy(showLocationRationale = action.showLocationPermissionRationale)
             }
 
