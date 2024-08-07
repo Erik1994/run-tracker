2001ea1b3291f631435ebd43b8273c8104015d10
Erik Antonyan <46004254+Erik1994@users.noreply.github.com>
2024-08-07 23:27:44 +0400

    Implement messaging clients for phone and watch to create a connection

--- run/data/src/main/java/com/example/run/data/connectivity/PhoneToWatchConnector.kt
+++ run/data/src/main/java/com/example/run/data/connectivity/PhoneToWatchConnector.kt
@@ -1,19 +1,33 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
 package com.example.run.data.connectivity
 
 import com.example.core.connectivity.domain.DeviceNode
 import com.example.core.connectivity.domain.DeviceType
 import com.example.core.connectivity.domain.NodeDiscovery
+import com.example.core.connectivity.domain.messaging.MessagingAction
+import com.example.core.connectivity.domain.messaging.MessagingClient
+import com.example.core.connectivity.domain.messaging.MessagingError
+import com.example.core.domain.util.EmptyResult
 import com.example.run.domain.WatchConnector
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.shareIn
 
 class PhoneToWatchConnector(
     nodeDiscovery: NodeDiscovery,
-    applicationScope: CoroutineScope
+    applicationScope: CoroutineScope,
+    private val messagingClient: MessagingClient
 ): WatchConnector {
 
     private val isTrackable = MutableStateFlow(false)
@@ -21,16 +35,50 @@ class PhoneToWatchConnector(
 
     override val connectedDevice: StateFlow<DeviceNode?> = _connectedNode.asStateFlow()
 
-    val messagingActions = nodeDiscovery
+    override val messagingActions:Flow<MessagingAction> = nodeDiscovery
         .observeConnectedDevices(DeviceType.PHONE)
-        .onEach { connectedDevices ->
+        .flatMapLatest { connectedDevices ->
             val node = connectedDevices.firstOrNull()
             if (node != null && node.isNearby) {
                 _connectedNode.value = node
+                messagingClient.connectToNode(node.id)
+            } else flowOf()
+        }
+        .onEach { messagingAction ->  
+            if (messagingAction == MessagingAction.ConnectionRequest) {
+                if (isTrackable.value) {
+                    sendActionToWatch(MessagingAction.Trackable)
+                } else {
+                    sendActionToWatch(MessagingAction.Untrackable)
+                }
             }
         }
-        .launchIn(applicationScope)
+        .shareIn(
+            applicationScope,
+            SharingStarted.Eagerly
+        )
 
+    init {
+        _connectedNode
+            .filterNotNull()
+            .flatMapLatest {
+                isTrackable
+            }
+            .onEach { isTrackable ->
+                sendActionToWatch(MessagingAction.ConnectionRequest)
+                val action = if (isTrackable) {
+                    MessagingAction.Trackable
+                } else {
+                    MessagingAction.Untrackable
+                }
+                sendActionToWatch(action)
+            }
+            .launchIn(applicationScope)
+    }
+
+    override suspend fun sendActionToWatch(action: MessagingAction): EmptyResult<MessagingError> {
+        return messagingClient.sendOrQueueAction(action)
+    }
 
     override fun setIsTrackable(isTrackable: Boolean) {
         this.isTrackable.value = isTrackable
